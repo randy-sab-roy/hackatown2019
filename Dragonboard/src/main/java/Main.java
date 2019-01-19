5560b6dcc4d980a5596585f02c59b0943dae0f32
Philippe Rivest <technophil98@gmail.com>
2019-01-19 17:38:11 -0500

    Added gitignore for java project

---
+++ Dragonboard/src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created on: 2019-01-19 at 17:37
+ *
+ * @author technophil98
+ */
+public class Main {
+
+   public static void main(String[] args) {
+      System.out.println("Hello World");
+   }
+
+}
